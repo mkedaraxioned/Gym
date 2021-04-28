798db36d2ddce38e807f25115c5b12b7fe5abb86
mkedaraxioned <kedarm@prdxn.com>
2021-04-28 18:26:43 +0530

    form validation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,6 +102,110 @@ $(document).ready(function() {
   start_counter4();   
   
   
+// form validation
+
+var form = $(".contact-form");
+form.on('submit', validateForm);
+var form_ip=$('.form-ip');
+var inputs=['fname','lname','subject','email','message'];
+var persons= {};
+var emailerr;
+var re_email = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+form_ip.each(function(){
+    $(this).on("input",validateField);
+});
+
+function validateField() {
+    var input_data = $(this).val();
+    console.log("this inp="+input_data);
+    var data_attr=$(this).attr("data-attr");
+    console.log("data attr "+data_attr);
+    if(data_attr=="email")
+    {
+        var is_email=re_email.test(input_data);
+        var err_class='.'+data_attr+'-error';
+        var err_span=$(err_class);
+        if(!is_email) {
+            err_span.html("Please enter "+data_attr); //ch1
+            err_span.addClass('show-element');
+            $(this).addClass('outline-red');
+            emailerr=1;                  
+         }
+        else {
+            emailerr=0;
+
+             if(err_span.hasClass('show-element'))
+            {
+              err_span.removeClass('show-element');
+              err_span.addClass('hide-element');                  
+            }
+            if($(this).hasClass("outline-red"))
+            {
+                $(this).removeClass("outline-red");
+            }
+
+        }
+    }
+
+    if(input_data=="")
+    {
+        var err_class='.'+data_attr+'-error';
+        var err_span=$(err_class);
+        err_span.html("Please enter "+data_attr); 
+        err_span.addClass('show-element')
+        $(this).addClass('outline-red');
+    }    
+}
+
+function validateForm(event) {
+    event.preventDefault();
+    form_ip.each(function(index){
+        persons[inputs[index]]=$(this).val();
+    });
+    var formFlag=0;
+
+    for(var i = 0 ; i < inputs.length; i++)
+    {   
+        if(persons[inputs[i]]=="")
+        {
+            var err_class='.'+inputs[i]+'-error';
+            var err_span=$(err_class);
+            err_span.html("Please enter "+inputs[i]); 
+            err_span.addClass('show-element');
+            var current_form_ip=$('.form-ip:nth-of-type('+i+')');
+            current_form_ip.addClass('outline-red');
+            formFlag=1; 
+        }
+    }
+    submitform();
+
+    function submitform(){
+
+        if((formFlag===0)&&(emailerr===0)) 
+        {
+          var span_err_shown=$(".span-error");
+          var form_ip=$('.form-ip');
+          span_err_shown.each(function(){
+            if($(this).hasClass('show-element'))
+            {
+                $(this).removeClass('show-element');
+                $(this).addClass('hide-element');                  
+            }
+        });
+        form_ip.each(function(){
+            if($(this).hasClass('outline-red'))
+            {
+                $(this).removeClass('outline-red');
+            }
+        });
+          var form_ip=$('.form-ip');
+        form_ip.each(function(){
+            $(this).val("");
+        });
+
+        }
+    }
+  }
 
 
   
